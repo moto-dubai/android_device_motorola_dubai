3de41b9879b0af6a5aec50c7ea1ca65644a85b00
Bruno Martins <bgcngm@gmail.com>
2025-01-27 09:45:36 +0000

    dubai: Rename rootdir to init and migrate to blueprints
    Change-Id: I9d55dc634fc4df2c60d30a0c18ec356a857e3c4b

--- device.mk
+++ device.mk
@@ -52,12 +52,9 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set,surfaceflinger,udfps_lib,//$(LOCAL_PATH):libudfps_extension.dubai)
 
 # Init
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
+PRODUCT_PACKAGES += \
+    init.mmi.overlay.rc \
+    init.vendor.st21nfc.rc
 
 # Keylayout
 PRODUCT_COPY_FILES += \
